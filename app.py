26f762bf170a45969bd456da1adf82b54defc639
adam01140 <achaaban@ucsc.edu>
2024-04-12 16:22:37 -0700

    edit all at once

--- app.py
+++ app.py
@@ -17,17 +17,12 @@ def upload_pdf():
         field_names = list(fields.keys()) if fields else []
         return jsonify({'fields': field_names})
     except Exception as e:
-        print(f"Upload Error: {e}")
         return jsonify({'error': str(e)}), 500
 
-@app.route('/edit_field', methods=['POST'])
-def edit_field():
+@app.route('/edit_pdf', methods=['POST'])
+def edit_pdf():
     try:
-        field_name = request.form['fieldName']
-        field_value = request.form['fieldValue']
         pdf_file = request.files['pdf']
-        print(f"Editing field {field_name} to {field_value} in the PDF.")
-
         reader = PyPDF2.PdfReader(pdf_file.stream)
         writer = PyPDF2.PdfWriter()
 
@@ -35,10 +30,11 @@ def edit_field():
             if '/Annots' in page:
                 annots = page['/Annots']
                 for annot in annots:
-                    resolved_annot = annot.get_object()  # Resolve the indirect object
-                    if resolved_annot.get('/T') == field_name:
+                    resolved_annot = annot.get_object()
+                    field_name = resolved_annot.get('/T')
+                    if field_name in request.form:
                         resolved_annot.update({
-                            PyPDF2.generic.NameObject('/V'): PyPDF2.generic.TextStringObject(field_value)
+                            PyPDF2.generic.NameObject('/V'): PyPDF2.generic.TextStringObject(request.form[field_name])
                         })
             writer.add_page(page)
 
@@ -48,7 +44,6 @@ def edit_field():
 
         return send_file(output_stream, as_attachment=True, download_name='modified_pdf.pdf')
     except Exception as e:
-        print(f"Edit Field Error: {e}")
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
